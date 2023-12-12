4dd81d0483175f57e4e5658b07163d3f02f9f3b1
Michael Pinskiy <mihapin10@gmail.com>
2023-12-12 20:03:19 +0200

    done

--- token_auth/index.js
+++ token_auth/index.js
@@ -1,33 +1,39 @@
-const uuid = require('uuid');
-const express = require('express');
-const onFinished = require('on-finished');
-const bodyParser = require('body-parser');
-const path = require('path');
-const port = 3000;
-const fs = require('fs');
+const uuid = require("uuid");
+const express = require("express");
+const onFinished = require("on-finished");
+const bodyParser = require("body-parser");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const SESSION_KEY = 'Authorization';
+const SECRET_KEY = "your_secret_key";
+const port = 3000;
+const SESSION_KEY = "Authorization";
 
 class Session {
-    #sessions = {}
+    #sessions = {};
 
     constructor() {
         try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
+            this.#sessions = fs.readFileSync("./sessions.json", "utf8");
             this.#sessions = JSON.parse(this.#sessions.trim());
 
             console.log(this.#sessions);
-        } catch(e) {
+        } catch (e) {
             this.#sessions = {};
         }
     }
 
     #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
+        fs.writeFileSync(
+            "./sessions.json",
+            JSON.stringify(this.#sessions),
+            "utf-8"
+        );
     }
 
     set(key, value) {
@@ -84,54 +90,52 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     if (req.session.username) {
         return res.json({
             username: req.session.username,
-            logout: 'http://localhost:3000/logout'
-        })
+            logout: "http://localhost:3000/logout",
+        });
     }
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
     sessions.destroy(req, res);
-    res.redirect('/');
+    res.redirect("/");
 });
 
 const users = [
     {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
+        login: "Login",
+        password: "Password",
+        username: "Username",
     },
     {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
-    }
-]
+        login: "Login1",
+        password: "Password1",
+        username: "Username1",
+    },
+];
 
-app.post('/api/login', (req, res) => {
+app.post("/api/login", (req, res) => {
     const { login, password } = req.body;
 
     const user = users.find((user) => {
         if (user.login == login && user.password == password) {
             return true;
         }
-        return false
+        return false;
     });
 
     if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-
-        res.json({ token: req.sessionId });
+        const token = jwt.sign({ username: user.username, login: user.login }, SECRET_KEY);
+        res.json({ token: token });
+    } else {
+        res.status(401).send();
     }
-
-    res.status(401).send();
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
